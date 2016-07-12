a41805b35866f86c4bfb0586923dc5e4defbc0a8
RonaldArthas <291559821@qq.com>
2016-07-12 08:35:18 +0800

    update-nightmode
    update-nightmode

--- AutoHome/AutoHome/ViewControllers/Me/Controller/XHQMeViewController.m
+++ AutoHome/AutoHome/ViewControllers/Me/Controller/XHQMeViewController.m
@@ -8,16 +8,113 @@
 
 #import "XHQMeViewController.h"
 
-@interface XHQMeViewController ()
-
+@interface XHQMeViewController ()<UITableViewDataSource, UITableViewDelegate>
 @end
 
 @implementation XHQMeViewController
-
+//- (instancetype)init
+//{
+//    self = [super initWithTagViewHeight:44];
+//    if (self) {
+//        
+//    }
+//    return self;
+//}
+//
+//
+//
+//
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.navigationItem.title = @"我";
+    
+//    self.navigationItem.title = @"我";
+//Set rightBarButton With Image Photos;
+    UIButton *modeBarButton = [UIButton buttonWithType:(UIButtonTypeCustom)];
+    [modeBarButton setBackgroundImage:[UIImage imageNamed:@"me_moon@2x.png"] forState:(UIControlStateNormal)];
+    modeBarButton.frame = CGRectMake(0, 0, 35, 35);
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:modeBarButton];
+//****************** 农大神
+    [modeBarButton addTarget:self action:@selector(nightMode:) forControlEvents:UIControlEventTouchUpInside];
+    [modeBarButton setBackgroundImage:[UIImage imageNamed:@"me_day@2x.png"] forState:UIControlStateSelected];
+    [self.view addSubview:modeBarButton];
+    
+    
+//    modeBarButton = [[UIBarButtonItem alloc]
+//    initWithImage:[UIImage imageNamed:@"me_moon@2x.png"] style:UIBarButtonItemStylePlain target:self action:@selector(nightMode:)] ;
+ 
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"我" style:(UIBarButtonItemStyleDone) target:self action:nil];
 }
+
+
+-(void)nightMode:(UIButton*)sender{
+    if ((sender.selected = !sender.selected))
+    {
+    self.view.backgroundColor = [UIColor grayColor];
+    }
+    else
+    {
+    self.view.backgroundColor = [UIColor whiteColor];
+    }
+//    self.view.backgroundColor = [UIColor grayColor];
+    
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section == 1) {
+        return 4;
+    }
+    return 1;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+//    
+//    switch (indexPath.section) {
+//        case 0:
+//            <#statements#>
+//            break;
+//            
+//        default:
+//            break;
+    }
+    
+    
+    
+    
+    
+}
+
+//-(void)viewDidAppear:(BOOL)animated
+//{
+//    [super viewDidAppear:animated];
+//    [self.navigationController setNavigationBarHidden:YES];
+//    self.automaticallyAdjustsScrollViewInsets = NO;
+//}
+//
+//
+
+//
+//-(void)initUI
+//{
+//    
+//    self.tagItemSize = CGSizeMake(80, 44);
+//    self.normalTitleColor = [UIColor grayColor];
+//    self.normalTitleFont  = [UIFont systemFontOfSize:LYNormalTitleFont];
+//    
+//    self.selectedTitleColor = LYBlueColor;
+//    self.selectedTitleFont = [UIFont systemFontOfSize:LYSelectedTitleFont];
+//    
+//    self.selectedIndicatorColor = LYBlueColor;
+//    
+//    NSArray *arrTitle = @[@"精选推荐",@"热帖",@"常用论坛"];
+//    NSArray *arrClass = @[[XHQJingXuanViewController class],[XHQReTieViewController class], [XHQChangYongForumViewController class]];
+//    
+//    [self reloadDataWith:arrTitle andSubViewdisplayClasses:arrClass withParams:nil];
+//}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
